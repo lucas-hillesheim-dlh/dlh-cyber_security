65a4c141279b0dc572b5cb14db1484f813a89c6b
Lucas Hillesheim <lucasgustavo.hillesheim@cyber.dlh.lu>
2026-05-15 18:43:41 +0200

    Crack the password using John the Ripper based on the file hash.txt

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --format=Raw-MD5 --wordlist=usr/share/wordlists/rockyou.txt $1
-john --show --format=Raw-MD5 hash.txt | grep ":" | awk -F':' '{print $2}' >  4-password.txt
+john --format=Raw-MD5 --wordlist=/usr/share/wordlists/rockyou.txt $1 && john --show --format=Raw-MD5 hash.txt | grep ":" | awk -F':' '{print $2}' >  4-password.txt
