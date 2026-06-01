42752b23dfeab5025540b59494eecb7e4f120833
Lucas Hillesheim <lucasgustavo.hillesheim@cyber.dlh.lu>
2026-06-01 18:28:51 +0200

    Script that retrieves and displays HTTP response headers from a website

---
+++ scripting_cyber/0x01-python_scripting/3-http_headers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import requests
+
+def get_http_headers(url):
+    try:
+        response = requests.get(url)
+
+        return {
+            "status_code": response.status_code,
+            "headers": dict(response.headers)
+        }
+    except requests.exceptions.RequestException:
+        return None
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    print(get_http_headers(url))
+
