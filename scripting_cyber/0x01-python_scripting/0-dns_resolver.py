0441331497ffb06999607966844e65702caf9848
Lucas Hillesheim <lucasgustavo.hillesheim@cyber.dlh.lu>
2026-06-01 17:06:45 +0200

    Script that resolves a domain name to its IPv4 address using the socket library

--- scripting_cyber/0x01-python_scripting/0-dns_resolver.py
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-from socket import gethostbyname, gaierror
-from sys import argv
+import socket
+import sys
 
 def resolve_domain_to_ipv4(domain):
     try:
-        result = gethostbyname(domain)
-    except gaierror as e:
+        result = socket.gethostbyname(domain)
+    except socket.gaierror as e:
         result = ""
     return result
 
 if __name__ == "__main__":
-    domain = argv[1]
+    domain = socket.argv[1]
     resolve_domain_to_ipv4(domain)
