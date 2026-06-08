3c0da26a8a5c57aa04c60d3b72ffcb455282e406
Lucas Hillesheim <lucasgustavo.hillesheim@cyber.dlh.lu>
2026-06-08 15:21:15 +0200

    Script that decode XOR WebSphere

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-e=`echo -n ${1#\{xor\}} | base64 -d`
-len=${#e}
-i=0
-
-while [ "$i" -lt "$len" ]
-do
-	char=$(($(printf %d "'${e:$i:1}") ^ 95))	
-	printf \\$(printf '%03o' $char)
-	i=$((i + 1))
-done
+echo "$1" | sed 's/^{xor}//' | base64 -d | perl -pe 's/(.)/$1 ^ "_"/ge'
