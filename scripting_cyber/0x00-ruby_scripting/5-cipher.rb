454f2655bef7a3934e596b08ea462ff29acf1efa
Lucas Hillesheim <lucasgustavo.hillesheim@cyber.dlh.lu>
2026-09-04 17:40:50 +0200

    5. Implementing a Caesar Cipher Encryption and Decryption using Classes

---
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+class CaesarCipher
+  def initialize(shift)
+    @shift = shift
+  end
+
+  def encrypt(message)
+    cipher(message, @shift)
+  end
+
+  def decrypt(message)
+    cipher(message, -@shift)
+  end
+
+  private
+
+  def cipher(message, shift)
+    message.chars.map do |char|
+      if char.between?('a', 'z')
+        (((char.ord - 'a'.ord + shift) % 26) + 'a'.ord).chr
+      elsif char.between?('A', 'Z')
+        (((char.ord - 'A'.ord + shift) % 26) + 'A'.ord).chr
+      else
+        char
+      end
+    end.join
+  end
+end
