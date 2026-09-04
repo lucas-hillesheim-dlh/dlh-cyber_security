db2e6a237cc02779289f21cbc3c26303d4486399
Lucas Hillesheim <lucasgustavo.hillesheim@cyber.dlh.lu>
2026-09-04 18:04:19 +0200

    10. Password Cracker

---
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+require 'digest'
+
+if ARGV.length != 2
+  puts "Usage: #{File.basename(__FILE__)} HASHED_PASSWORD DICTIONARY_FILE"
+  exit 1
+end
+
+target_hash = ARGV[0].downcase
+dictionary_file = ARGV[1]
+
+unless File.exist?(dictionary_file)
+  puts "Password not found in dictionary."
+  exit 1
+end
+
+found_password = nil
+
+File.foreach(dictionary_file) do |line|
+  word = line.chomp
+  if Digest::SHA256.hexdigest(word).downcase == target_hash
+    found_password = word
+    break
+  end
+end
+
+if found_password
+  puts "Password found: #{found_password}"
+else
+  puts "Password not found in dictionary."
+end
