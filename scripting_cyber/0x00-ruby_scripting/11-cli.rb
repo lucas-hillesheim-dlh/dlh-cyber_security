b9a4f68e15353f55fa81117b09c6202dfa52da4c
Lucas Hillesheim <lucasgustavo.hillesheim@cyber.dlh.lu>
2026-09-06 10:46:14 +0200

    11. Creating a Basic CLI Application

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -49,6 +49,7 @@ elsif options[:list]
   if tasks.empty?
     puts "No tasks found."
   else
+    puts "Tasks:"
     tasks.each_with_index do |task, index|
       puts "#{index + 1}. #{task}"
     end
