5cbff4b4366b49c73183b104426dddad47ef5fe2
Lucas Hillesheim <lucasgustavo.hillesheim@cyber.dlh.lu>
2026-09-06 10:40:26 +0200

    8. HTTP POST Request

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,10 +1,20 @@
 #!/usr/bin/env ruby
 require 'net/http'
 require 'uri'
+require 'json'
 
 def post_request(url, body_params = {})
   uri = URI.parse(url)
-  response = Net::HTTP.post_form(uri, body_params)
+  
+  # Create the request object
+  request = Net::HTTP::Post.new(uri)
+  request['Content-Type'] = 'application/json'
+  request.body = body_params.to_json
+
+  # Send the request
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    http.request(request)
+  end
 
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
